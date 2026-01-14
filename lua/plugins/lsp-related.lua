27b7ba4a924c172daae34c24bc65b785bffafefe
João Pedro <joao9edroo@gmail.com>
2026-01-14 08:18:17 -0300

    - Melhoria nas ferramentas de LSP
    - Lazy Loading com base em eventos
    
    - Dashboard adicionado

--- lua/plugins/lsp-related.lua
+++ lua/plugins/lsp-related.lua
@@ -1,15 +1,65 @@
 return{
-	{'neovim/nvim-lspconfig'},
+	{'neovim/nvim-lspconfig', lazy = false},
 	{'mason-org/mason.nvim', opts={}, keys={
 		{"<leader>m", ":Mason<CR>", desc="Abre o mason"}
 	}
 },
-	{'mason-org/mason-lspconfig.nvim', opts = {}},
-	{'hrsh7th/cmp-nvim-lsp', enabled = true},
-	{'hrsh7th/nvim-cmp', opts={}, enabled = true},
-	{'nvim-mini/mini.completion', enabled=false,opts={}},
-	{'saghen/blink.cmp', version = '1.*', enabled=false, opts={
-		keymap = { preset = 'default' },
-		fuzzy = { implementation = "lua"}
-	}}
+{'mason-org/mason-lspconfig.nvim', opts = {} , event = {"BufReadPost", "BufNewFile" } },
+{'hrsh7th/cmp-nvim-lsp', enabled = true, event ={"BufReadPost", "BufNewFile" }},
+{'hrsh7th/nvim-cmp', opts={}, enabled = true, event = {"InsertEnter"},
+config = function()
+	local borda = {
+		{ '❀' }, --Borda superior esquerda
+		{ '─' },
+		{ '✶' }, --Borda superior direita
+		{ '│' },
+		{ '❀' }, --Borda inferior direita
+		{ '─' },
+		{ '☻' }, --Borda Inferior esquerda
+		{ '│' },
+	}
+	-- Lsp
+	local cmp = require('cmp')
+	cmp.setup({
+		window = {
+			completion = cmp.config.window.bordered({ border = borda }),
+			documentation = cmp.config.window.bordered({ border = borda })
+		},
+		mapping = {
+			['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+			['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+			['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+			['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+			['<C-d>'] = cmp.mapping.scroll_docs(-4),
+			['<C-f>'] = cmp.mapping.scroll_docs(4),
+			['<C-Space>'] = cmp.mapping.complete(),
+			['<C-e>'] = cmp.mapping.close(),
+			['<CR>'] = cmp.mapping.confirm({
+				behavior = cmp.ConfirmBehavior.Replace,
+				select = true,
+			}),
+			['<tab>'] = function ()
+				if cmp.visible() then
+					cmp.select_next_item()
+				end 
+			end,
+			['<S-tab>'] = function ()
+				if cmp.visible() then
+					cmp.select_prev_item()
+				end
+			end,
+		},
+		sources = {
+			{ name = 'nvim_lsp' }
+		}
+	})
+
+
+end
+},
+{'nvim-mini/mini.completion', enabled=false,opts={}},
+{'saghen/blink.cmp', version = '1.*', enabled=false, opts={
+	keymap = { preset = 'default' },
+	fuzzy = { implementation = "lua"}
+}}
 }
--- lua/plugins/petiscos.lua
+++ lua/plugins/petiscos.lua
@@ -1,23 +1,63 @@
+local getWindows = vim.loop.os_uname().sysname == "Windows_NT"
+-- Teste
+-- print(getWindows)
+-- print(vim.loop.os_uname().sysname)
+local configuracao = vim.fn.stdpath("config")
+local mascotePath 
+if getWindows then
+	mascotePath = configuracao .. '\\mascote\\carnebuzina.png'
+else
+	mascotePath = configuracao .. '/mascote/carnebuzina.png; sleep .1'
+end
+
 return {
 	'folke/snacks.nvim',
 	lazy = false,
 	opts = {
 		dashboard = {
 			enabled = true,
-			width = 70,
-			pane_gap = 4,
+			width = 60,
+			col = nil,
+			row = nil,
+			pane_gap = 2,
+			preset = {
+				keys = {
+					{ icon = "", key = "f", desc = "Encontrar arquivos", action = ":lua Snacks.dashboard.pick('files')" },
+					{ icon = "✎", key = "n", desc = "Novo arquivo", action = ":ene | startinsert" },
+					{ icon = "🗎", key = "g", desc = "Grep arquivos", action = ":lua Snacks.dashboard.pick('live_grep')" },
+					{ icon = "⚙", key = "c", desc = "Configurações do neovim", action = ":lua Snacks.dashboard.pick('files', { cwd = vim.fn.stdpath('config')})" },
+					{ icon = "ᶻ 𝗓 𐰁", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+					{ icon = "☺", key = "q", desc = "Sair", action = ":qa" },
+				},
+			},
 			sections = {
 				{
-
 					section = "terminal",
-					cmd = 'chafa --size 48x48 --stretch --dither ordered "'  .. vim.fn.stdpath("config") .. '"/mascote/CARNEbuzina.png"',
-					padding = 2,
-					height = 48
+					cmd = 'chafa -c full --stretch --align center,center --size 32x32 ' .. mascotePath,
+					height = 32,
+					padding = 1,
+					gap = 1,
+					{ title = " __〆(￣ー￣ ) ", align = "center" },
+					{ section = "startup" },
+				},
+
+				{
+					pane = 2,
+					{
+						section = "terminal",
+						enabled = function()
+							return Snacks.git.get_root() ~= nil
+						end,
+						cmd = "git log --all --decorate --oneline --graph",
+						height = 25,
+						{ section = "keys", gap = 1, padding = 4}
+					},
+
 				},
 
 			},
-
 		},
+
 		picker = {
 			enabled = true
 		},
@@ -29,15 +69,13 @@ return {
 			}
 
 		},
-
 	},
 	keys = {
-		{ "<leader>ff", function() Snacks.picker.files() end,                                 desc = "Procurar arquivos :)" },
-		{ "<leader>fg", function() Snacks.picker.grep() end,                                  desc = "Grep no diretório" },
-		{ "<leader>fb", function() Snacks.picker.buffers() end,                               desc = "Lista Buffers" },
-		{ "<leader>fc", function() Snacks.picker.colorschemes() end,                          desc = "Picker de temas" },
+		{ "<leader>ff", function() Snacks.picker.files() end,                                   desc = "Procurar arquivos :)" },
+		{ "<leader>fg", function() Snacks.picker.grep() end,                                    desc = "Grep no diretório" },
+		{ "<leader>fb", function() Snacks.picker.buffers() end,                                 desc = "Lista Buffers" },
+		{ "<leader>fc", function() Snacks.picker.colorschemes() end,                            desc = "Picker de temas" },
 		{ "<leader>ns", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Neovim Config." },
-		{ "<leader>fh", function() Snacks.picker.help() end,                                  desc = "Páginas de ajuda" }
+		{ "<leader>fh", function() Snacks.picker.help() end,                                    desc = "Páginas de ajuda" }
 	},
-
 }
